53b5091f592e78da696ea1576cbc30d28caedb0a
Davin-Aristia <davin.aristia81@gmail.com>
2023-11-18 16:59:45 +0700

    [FEAT] get, post, and delete endpoint for user-forum

--- database/database.go
+++ database/database.go
@@ -59,6 +59,7 @@ func InitialMigration() {
 	DB.AutoMigrate(&model.User{}, &model.Doctor{})
 	DB.AutoMigrate(&model.User{}, &model.Doctor{})
 	DB.AutoMigrate(&model.Patient{})
+	DB.AutoMigrate(&model.Forum{})
 	DB.AutoMigrate(&model.Specialist{})
 	DB.AutoMigrate(&model.Forum{})
 }
@@ -96,6 +97,7 @@ func AdminSeeders() {
 			DB.Create(&v)
 		}
 	}
+	DB.AutoMigrate(&model.Forum{})
 }
 
 func AdminSeeders() {
